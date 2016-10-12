c4ab1b62c5407e1955851db2b5abbeae237ab134
Benson Yang <bensonyang1999@gmail.com>
2016-10-11 20:05:19 -0700

    add test auto

--- src/auto.c
+++ src/auto.c
@@ -2,6 +2,23 @@
 
 #include "auto.h"
 
+void driveFB(int drive)
+{
+    vexMotorSet(kVexMotor_4, drive);
+    vexMotorSet(kVexMotor_7, drive);
+    vexMotorSet(kVexMotor_5, drive);
+    vexMotorSet(kVexMotor_6, drive);
+}
+
+void setLift(int lift)
+{
+    vexMotorSet(kVexMotor_2, lift);
+    vexMotorSet(kVexMotor_3, lift);
+    vexMotorSet(kVexMotor_8, lift);
+    vexMotorSet(kVexMotor_9, lift);
+}
+
+
 /*-----------------------------------------------------------------------------*/
 /** @brief      Autonomous                                                     */
 /*-----------------------------------------------------------------------------*/
@@ -18,8 +35,23 @@ vexAutonomous( void *arg )
 
     while(1)
         {
-        // Don't hog cpu
-        vexSleep( 25 );
+            driveFB(127);
+            vexSleep(750);
+            driveFB(0);
+            vexSleep(150);
+            driveFB(-127);
+            vexSleep(500);
+            driveFB(0);
+            setLift(127);
+            vexSleep(500);
+            setLift(0);
+            vexSleep(150);
+            setLift(-127);
+            vexSleep(350);
+            setLift(0);
+            vexSleep(12500);
+            // Don't hog cpu
+            vexSleep( 25 );
         }
 
     return (msg_t)0;
