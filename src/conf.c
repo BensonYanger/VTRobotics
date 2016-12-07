2c826718e43262fc249f1f4bf18feb9358000e01
Benson Yang <bensonyang1999@gmail.com>
2016-12-07 00:12:25 -0800

    space conf to make it look nice

--- src/conf.c
+++ src/conf.c
@@ -4,18 +4,18 @@
 
 // Digi IO configuration
 static  vexDigiCfg  dConfig[kVexDigital_Num] = {
-        { kVexDigital_1,    kVexSensorDigitalOutput,  kVexConfigOutput,      0 },
-        { kVexDigital_2,    kVexSensorDigitalOutput,  kVexConfigOutput,      0 },
-        { kVexDigital_3,    kVexSensorDigitalOutput,  kVexConfigOutput,      0 },
-        { kVexDigital_4,    kVexSensorDigitalInput,  kVexConfigInput,      0 },
-        { kVexDigital_5,    kVexSensorDigitalInput,  kVexConfigInput,      0 },
-        { kVexDigital_6,    kVexSensorQuadEncoder,  kVexConfigQuadEnc1,      kVexQuadEncoder_1 },
-        { kVexDigital_7,    kVexSensorQuadEncoder,  kVexConfigQuadEnc2,      kVexQuadEncoder_1 },
-        { kVexDigital_8,    kVexSensorQuadEncoder,  kVexConfigQuadEnc1,      kVexQuadEncoder_2 },
-        { kVexDigital_9,    kVexSensorQuadEncoder,  kVexConfigQuadEnc2,      kVexQuadEncoder_2 },
-        { kVexDigital_10,   kVexSensorDigitalOutput, kVexConfigOutput,     0 },
-        { kVexDigital_11,   kVexSensorQuadEncoder,  kVexConfigQuadEnc1,      kVexQuadEncoder_3 },
-        { kVexDigital_12,   kVexSensorQuadEncoder,  kVexConfigQuadEnc2,      kVexQuadEncoder_3 }
+        { kVexDigital_1,    kVexSensorDigitalOutput,  kVexConfigOutput,     0 },
+        { kVexDigital_2,    kVexSensorDigitalOutput,  kVexConfigOutput,     0 },
+        { kVexDigital_3,    kVexSensorDigitalOutput,  kVexConfigOutput,     0 },
+        { kVexDigital_4,    kVexSensorDigitalInput,   kVexConfigInput,      0 },
+        { kVexDigital_5,    kVexSensorDigitalInput,   kVexConfigInput,      0 },
+        { kVexDigital_6,    kVexSensorQuadEncoder,    kVexConfigQuadEnc1,   kVexQuadEncoder_1 },
+        { kVexDigital_7,    kVexSensorQuadEncoder,    kVexConfigQuadEnc2,   kVexQuadEncoder_1 },
+        { kVexDigital_8,    kVexSensorQuadEncoder,    kVexConfigQuadEnc1,   kVexQuadEncoder_2 },
+        { kVexDigital_9,    kVexSensorQuadEncoder,    kVexConfigQuadEnc2,   kVexQuadEncoder_2 },
+        { kVexDigital_10,   kVexSensorDigitalOutput,  kVexConfigOutput,     0 },
+        { kVexDigital_11,   kVexSensorQuadEncoder,    kVexConfigQuadEnc1,   kVexQuadEncoder_3 },
+        { kVexDigital_12,   kVexSensorQuadEncoder,    kVexConfigQuadEnc2,   kVexQuadEncoder_3 }
 };
 
 static  vexMotorCfg mConfig[kVexMotorNum] = {
