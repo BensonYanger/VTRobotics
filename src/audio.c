2c1ced979a57a40177c7fa738195b71c5e692ba0
Benson Yang <bensonyang1999@gmail.com>
2016-12-10 02:52:22 -0800

    slight comments and respacing of audio.c

--- src/audio.c
+++ src/audio.c
@@ -11,6 +11,7 @@ char *Mario = "Super Mario:d=4,o=5,b=100:16e6,16e6,32p,8e6,16c6,8e6,8g6,8p,8g,8p
                 "8p,16g6,16f#6,16f6,16d#6,16p,16e6,16p,16g#,16a,16c6,16p,16a,16c6,16d6,""8p,16d#6,8p,16d6,8p,16c6";
 char *Zelda = "Zelda1:d=4,o=5,b=125:a#,f.,8a#,16a#,16c6,16d6,16d#6,2f6,8p,8f6,16f.6,16f#6,16g#.6,2a#.6,16a#.6,16g#6,16f#.6,8g#.6,16f#.6,2f6,f6,8d#6,16d#6,16f6,2f#6,8f6,8d#6,8c#6,16c#6,16d#6,2f6,8d#6,8c#6,8c6,16c6,16d6,2e6,g6,8f6,16f,16f,8f,16f,16f,8f,16f,16f,8f,8f,a#,f.,8a#,16a#,16c6,16d6,16d#6,2f6,8p,8f6,16f.6,16f#6,16g#.6,2a#.6,c#7,c7,2a6,f6,2f#.6,a#6,a6,2f6,f6,2f#.6,a#6,a6,2f6,d6,2d#.6,f#6,f6,2c#6,a#,c6,16d6,2e6,g6,8f6,16f,16f,8f,16f,16f,8f,16f,16f,8f,8f";
 char *StarWars = "StarWars:d=4,o=5,b=45:32p,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#.6,32f#,32f#,32f#,8b.,8f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32c#6,8b.6,16f#.6,32e6,32d#6,32e6,8c#6";
+
 static WORKING_AREA(waSpeakerPress, 512);
 msg_t
 speakerPress(void *arg)
@@ -35,6 +36,7 @@ speakerPress(void *arg)
             vexAudioPlayRtttl(StarWars, 128, 0);
         }
 
+        /* XMTR 2 */
         if(vexControllerGet(Btn7UXmtr2)) {
             vexAudioPlayRtttl(Flinstones, 128, 0);
         }
